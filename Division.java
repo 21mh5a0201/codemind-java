3a4ea5f0d80db55008a2cef8215e4e91af3bcc41
21mh5a0201 <113753392+21mh5a0201@users.noreply.github.com>
2022-09-18 23:02:34 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         int a,b,d;         Scanner Sc=new Scanner(System.in);         a=Sc.nextInt();         b=Sc.nextInt();         d=a/b;         System.out.println(d);     } }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        int a,b,d;
+        Scanner Sc=new Scanner(System.in);
+        a=Sc.nextInt();
+        b=Sc.nextInt();
+        d=a/b;
+        System.out.println(d);
+    }
+}
